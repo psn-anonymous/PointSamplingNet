b3c2037c9b89d0e9ffee42a16daf548e45b438a5
psn-anonymous <vlb@foxmail.com>
2020-11-05 12:44:30 +0800

    Formatting

--- models/PointSamplingNet.py
+++ models/PointSamplingNet.py
@@ -5,7 +5,6 @@ from torch import Tensor
 from typing import List, Tuple
 
 
-
 class PointSamplingNet(nn.Module):
     """
     Point Sampling Net PyTorch Module.
@@ -16,7 +15,8 @@ class PointSamplingNet(nn.Module):
         mlp: the channels of feature transform function, List[int]
         global_geature: whether enable global feature, bool
     """
-    def __init__(self, num_to_sample: int= 512, max_local_num: int = 32, mlp: List[int] = [32, 64, 256], global_feature: bool = False) -> None:
+
+    def __init__(self, num_to_sample: int = 512, max_local_num: int = 32, mlp: List[int] = [32, 64, 256], global_feature: bool = False) -> None:
         """
         Initialization of Point Sampling Net.
         """
@@ -25,7 +25,7 @@ class PointSamplingNet(nn.Module):
         self.mlp_convs = nn.ModuleList()
         self.mlp_bns = nn.ModuleList()
 
-        assert len(mlp)>1, "The number of MLP layers must greater than 1 !"
+        assert len(mlp) > 1, "The number of MLP layers must greater than 1 !"
 
         self.mlp_convs.append(nn.Conv1d(3, mlp[0], 1))
         self.mlp_bns.append(nn.BatchNorm1d(mlp[0]))
@@ -42,8 +42,6 @@ class PointSamplingNet(nn.Module):
             self.mlp_convs.append(nn.Conv1d(mlp[-1] * 2, num_to_sample, 1))
         else:
             self.mlp_convs.append(nn.Conv1d(mlp[-1], num_to_sample, 1))
-
-
 
         self.softmax = nn.Softmax(1)
 
@@ -68,23 +66,20 @@ class PointSamplingNet(nn.Module):
 
         for i in range(len(self.mlp_convs) - 1):
             x = F.relu(self.mlp_bns[i](self.mlp_convs[i](x)))
-        
+
         if self.global_feature:
             max_feature = torch.max(x, 2, keepdim=True)[0]
-            max_feature = max_feature.repeat(1, 1, N)  #[B, mlp[-1], m]
-            x = torch.cat([x, max_feature], 1) #[B, mlp[-1]*2, m]
+            max_feature = max_feature.repeat(1, 1, N)  # [B, mlp[-1], m]
+            x = torch.cat([x, max_feature], 1)  # [B, mlp[-1]*2, m]
 
-        x = self.mlp_convs[-1](x)  #[B,S,m]
+        x = self.mlp_convs[-1](x)  # [B,S,m]
 
         Q = self.softmax(x)  # [B, S, N] 归一化
 
-        _, indices = torch.sort(input = Q, dim = 2, descending=True)  # [B, S, m]
+        _, indices = torch.sort(input=Q, dim=2, descending=True)  # [B, S, m]
 
         grouped_indices = indices[:, :, 0:self.n]  # [B, S, n]
 
-        sampled_indices = indices[:, :, 0] #[B, S]
+        sampled_indices = indices[:, :, 0]  # [B, S]
 
         return sampled_indices, grouped_indices
-
-
-
